a2e4b91c6413b30e2668a8ba73a9dab60004283a
norwingc <norwingcruz@gmail.com>
2014-12-21 18:49:11 -0600

    registro estilo desktop
    -estilo desktop
    -faltan imagenes

--- app/views/nosotros/registrar.blade.php
+++ app/views/nosotros/registrar.blade.php
@@ -32,66 +32,60 @@
 	<div class="campos">
 		{{ Form::open(array('url' => 'candidato/save', 'class' => 'form-horizontal')) }}
 			<div class="categoria cuenta">
-				<img src="{{ asset('img/bd.png') }}" alt="" class="computer">
-				<img src="{{ asset('img/bd.png') }}" alt="" class="computer">
+				<img src="{{ asset('img/bd.png') }}" alt="" class="computer img-user">
+				<img src="{{ asset('img/bd.png') }}" alt="" class="computer img-contra">
 				<h3 class="titulother">
 					<img src="{{ asset('img/bd.png') }}" alt="" class="computer">
 					Datos de la cuenta
 				</h3>
-				<div class="campo">
+				<div class="campo col-sm-offset-4">
 					<div class="form-group">						
-					    <div class="col-sm-5">
+					    <div class="col-sm-6">
 					      {{ Form::text('username', Input::old('username'), array('class' => 'form-control', 'placeholder'=> 'Usuario')) }}	
 					    </div>
 					</div>	
 					<div class="form-group">
-						<img src="{{ asset('img/bd.png') }}" alt="" class="computer">
-					    <div class="col-sm-5">
+					    <div class="col-sm-6">
 					      {{ Form::password('password', array('class' => 'form-control', 'placeholder'=> 'Contraseña')) }}	
 					    </div>
 					</div>	
 					<div class="form-group">
-						<img src="{{ asset('img/bd.png') }}" alt="" class="computer">
-					    <div class="col-sm-5">
+					    <div class="col-sm-6">
 					      {{ Form::password('password_confirmation', array('class' => 'form-control', 'placeholder'=> 'Confirmar Contraseña')) }}	
 					    </div>
 					</div>	
 				</div>
 			</div>
 			{{-- datos personales --}}
-			<div class="categoria personales">
+			<div class="categoria personales ">
 				<h3 class="titulother">
 					<img src="{{ asset('img/bd.png') }}" alt="" class="computer">
 					Datos Personales
 				</h3>
-				<div class="campo">
+				<div class="campo col-sm-offset-2">
 					<div class="form-group">						
 					    <div class="col-sm-5">
 					      {{ Form::text('nombres', Input::old('nombres'), array('class' => 'form-control', 'placeholder'=> '*Nombres')) }}	
-					    </div>
-					</div>	
-					<div class="form-group">						
+					    </div>										
 					    <div class="col-sm-5">
 					      {{ Form::text('apellidos', Input::old('apellidos'), array('class' => 'form-control', 'placeholder'=> '*Apellidos')) }}	
 					    </div>
 					</div>
 					<h3 class="subtitul">Fecha de nacimiento</h3>
 					<div class="form-group">						
-					    <div class="col-sm-5">
+					    <div class="col-sm-6 col-sm-offset-2">
 					      {{ Form::input('date', 'fecha_nacimiento', Input::old('fecha_nacimiento'), array('class' => 'form-control')) }}	
 					    </div>
 					</div>
 					<div class="form-group">						
-					    <div class="col-sm-5">
+					    <div class="col-sm-10">
 					      {{ Form::textarea( 'direccion', Input::old('direccion'), array('class' => 'form-control','placeholder'=> '*Direccion', 'id' => 'textareainput')) }}	
 					    </div>
 					</div>
 					<div class="form-group">						
 					    <div class="col-sm-5">
 					      {{ Form::email( 'email', Input::old('email'), array('class' => 'form-control','placeholder'=> '*Correo Electronico')) }}	
-					    </div>
-					</div>
-					<div class="form-group">						
+					    </div>										
 					    <div class="col-sm-5">
 					      {{ Form::email( 'email_confirmation', Input::old('email_confirmation'), array('class' => 'form-control','placeholder'=> '*Comprovar Correo electronico')) }}	
 					    </div>
@@ -104,9 +98,7 @@
 								 <option value="Casado">Casado</option>				
 								 <option value="Union libre">Union libre</option>				
 							</select> 
-					    </div>
-					</div>
-					<div class="form-group">						
+					    </div>											
 					    <div class="col-sm-5">
 					    	{{ Form::text('nacionalidad', Input::old('nacionalidad'), array('class' => 'form-control', 'placeholder'=> '*Nacionalidad')) }}	
 					    </div>
@@ -118,9 +110,7 @@
 								 <option value="Femenino">Femenino</option>				
 								 <option value="Masculino">Masculino</option>				
 							</select> 
-					    </div>
-					</div>
-					<div class="form-group">						
+					    </div>									
 					    <div class="col-sm-5">
 					    	<select class="form-control" name="departamento" >
 								 <option selected="selected" class="s">*Departamento</option>
@@ -137,9 +127,7 @@
 								 <option value="Pasaporte">Pasaporte</option>				
 								 <option value="Cedula de Residencia">Cedula de Residencia</option>				
 							</select> 
-					    </div>
-					</div>
-					<div class="form-group">						
+					    </div>											
 					    <div class="col-sm-5">
 					    	{{ Form::text('no_identificacion', Input::old('no_identificacion'), array('class' => 'form-control', 'placeholder'=> '*No. Identificacion')) }}	
 					    </div>
@@ -147,9 +135,7 @@
 					<div class="form-group">						
 					    <div class="col-sm-5">
 					    	{{ Form::text('convencional', Input::old('convencional'), array('class' => 'form-control', 'placeholder'=> '*Telefono de Casa', 'id' => 'convencional')) }}	
-					    </div>
-					</div>
-					<div class="form-group">						
+					    </div>											
 					    <div class="col-sm-5">
 					    	{{ Form::text('celular', Input::old('celular'), array('class' => 'form-control', 'placeholder'=> '*Telefono Celular', 'id' => 'celular')) }}	
 					    </div>
@@ -161,9 +147,7 @@
 								 <option value="0">Si</option>				
 								 <option value="1">No</option>	
 							</select> 
-					    </div>					    
-					</div>
-					<div class="form-group">	
+					    </div>
 					    <div class="col-sm-5">
 					    	<select class="form-control" name="disponible_viajar" id="pr">
 								 <option selected="selected" class="s">Disponibilidad de Viajar </option>
@@ -188,7 +172,7 @@
 					</div>
 					<h3 class="subtitul">Objetivos/Principales logros/Competencias</h3>
 					<div class="form-group">						
-					    <div class="col-sm-5">
+					    <div class="col-sm-10">
 					    	{{ Form::textarea('objetivo', Input::old('objetivo'), array('class' => 'form-control', 'placeholder'=> '*Escribe aqui tus objetivos')) }}	
 					    </div>
 					</div>
@@ -200,9 +184,9 @@
 					<img src="{{ asset('img/bd.png') }}" alt="" class="computer">
 					EXPECTATIVA LABORAL
 				</h3>
-				<div class="campo">
+				<div class="campo col-sm-offset-2">
 					<div class="form-group">	
-					    <div class="col-sm-5">
+					    <div class="col-sm-5 ">
 					    	<select class="form-control" name="interes_laboral" >
 								 <option selected="selected" class="s">*Nivel de Interes Laboral</option>
 								 <option value="Permanente">Permanente</option>				
@@ -212,9 +196,7 @@
 								 <option value="Pasantia">Pasantia</option>	
 								 <option value="Por proyecto">Por proyecto</option>	
 							</select> 
-					    </div>					    
-					</div>
-					<div class="form-group">	
+					    </div>
 					    <div class="col-sm-5">
 					    	<select class="form-control" name="expectativa_salarial" >
 								<option selected="selected" class="s">*Expectativa Salarial Mensual </option>
@@ -255,9 +237,7 @@
 								 <option value="Chinandega">Chinandega</option>	
 								 <option value="Chontales">Chontales</option>	
 							</select> 
-					    </div>					    
-					</div>
-					<div class="form-group">	
+					    </div>	
 					    <div class="col-sm-5">
 					    	<select class="form-control" name="areas_interes" >
 								 <option selected="selected" class="s">Areas de Interes </option>
@@ -281,9 +261,7 @@
 								 <option value="Jefe de Mercadeo">Jefe de Mercadeo</option>	
 								 <option value="Analista de Mercadeo|Investigacion de Mercado">Analista de Mercadeo|Investigacion de Mercado</option>	
 							</select> 
-					    </div>					    
-					</div>
-					<div class="form-group">	
+					    </div>	
 					    <div class="col-sm-5">
 					    	<select class="form-control" name="horario" >
 								 <option selected="selected" class="s">Horario </option>
@@ -303,13 +281,11 @@
 					<img src="{{ asset('img/bd.png') }}" alt="" class="computer">
 					EXPERIENCIA PROFESIONAL
 				</h3>
-				<div class="campo">
+				<div class="campo col-sm-offset-2">
 					<div class="form-group">						
 					    <div class="col-sm-5">
 					    	{{ Form::text('nombre_empresa', Input::old('nombre_empresa'), array('class' => 'form-control', 'placeholder'=> '*Nombre de la Empresa')) }}	
 					    </div>
-					</div>
-					<div class="form-group">	
 					    <div class="col-sm-5">
 					    	<select class="form-control" name="actividad_empresa" >
 								 <option selected="selected" class="s">*Actividad de la Empresa</option>
@@ -325,8 +301,6 @@
 					    <div class="col-sm-5">
 					    	{{ Form::text('telefono_empresa', Input::old('telefono_empresa'), array('class' => 'form-control', 'placeholder'=> '*Tefono de Empresa')) }}	
 					    </div>
-					</div>
-					<div class="form-group">	
 					    <div class="col-sm-5">
 					    	<select class="form-control" name="area" >
 								 <option selected="selected" class="s">*Area Funcional</option>
@@ -339,29 +313,28 @@
 					    </div>					    
 					</div>					
 					<div class="form-group">						
-					    <div class="col-sm-5">
+					    <div class="col-sm-6 col-sm-offset-2">
 					    	{{ Form::text('puesto', Input::old('puesto'), array('class' => 'form-control', 'placeholder'=> '*Puesto o Cargo Desempeñado')) }}	
 					    </div>
 					</div>
-					<h3 class="subtitul">Fecha Inicio</h3>						
+					
 					<div class="form-group">
-						<div class="col-sm-5">
+						<div class="col-sm-5 ">
+							<h3 class="subtitul" style="width: 100%">Fecha Inicio</h3>				
 					    	{{ Form::input('date','fecha_inicio', Input::old('fecha_inicio'), array('class' => 'form-control')) }}	
 					    </div>
-					</div>
-					<h3 class="subtitul">Fecha Fin</h3>					
-					<div class="form-group">	
-						<div class="col-sm-5">
+						<div class="col-sm-5 ">
+							<h3 class="subtitul" style="width: 100%">Fecha Fin</h3>				
 					    	{{ Form::input('date','fecha_fin', Input::old('fecha_fin'), array('class' => 'form-control')) }}	
 					    </div>
 					</div>
 					<div class="form-group">						
-					    <div class="col-sm-5">
+					    <div class="col-sm-10">
 					    	{{ Form::textarea('logros', Input::old('logros'), array('class' => 'form-control', 'placeholder'=> 'Logros')) }}	
 					    </div>
 					</div>
 					<div class="form-group">						
-					    <div class="col-sm-5">
+					    <div class="col-sm-10">
 					    	{{ Form::textarea('funciones', Input::old('funciones'), array('class' => 'form-control', 'placeholder'=> '*Descripcion breve de principales funciones en el puesto')) }}	
 					    </div>
 					</div>
@@ -372,9 +345,7 @@
 								 <option value="0">Si</option>				
 								 <option value="1">No</option>	
 							</select> 
-					    </div>					    
-					</div>
-					<div class="form-group">						
+					    </div>			
 					    <div class="col-sm-5">
 					    	{{ Form::text('nombre_contacto', Input::old('nombre_contacto'), array('class' => 'form-control', 'placeholder'=> '*Nombre de Contacto')) }}	
 					    </div>
@@ -382,9 +353,7 @@
 					<div class="form-group">						
 					    <div class="col-sm-5">
 					    	{{ Form::text('telefono_contacto', Input::old('telefono_contacto'), array('class' => 'form-control', 'placeholder'=> '*Telefono de Contacto')) }}	
-					    </div>
-					</div>
-					<div class="form-group">						
+					    </div>			
 					    <div class="col-sm-5">
 					    	{{ Form::text('email_contacto', Input::old('email_contacto'), array('class' => 'form-control', 'placeholder'=> 'E-mail de Contacto')) }}	
 					    </div>
@@ -397,7 +366,7 @@
 					<img src="{{ asset('img/bd.png') }}" alt="" class="computer">
 					Educacion Academica
 				</h3>
-				<div class="campo">
+				<div class="campo col-sm-offset-2">
 					<div class="form-group">	
 					    <div class="col-sm-5">
 					    	<select class="form-control" name="nivel_academico" >
@@ -415,15 +384,8 @@
 								 <option value="Doctorado">Doctorado</option>				
 								 <option value="Sin Estudios Formales">Sin Estudios Formales</option>				
 							</select> 
-					    </div>					    
-					</div>
-					<div class="form-group">						
+					    </div>	
 					    <div class="col-sm-5">
-					    	{{ Form::text('titulo', Input::old('titulo'), array('class' => 'form-control', 'placeholder'=> '*Titulo')) }}	
-					    </div>
-					</div>
-					<div class="form-group">
-					   <div class="col-sm-5">
 					    	<select class="form-control" name="instituto" >
 								 <option selected="selected" class="s">*Institucion </option>
 								 <option value="Ave Maria University">Ave Maria University</option>				
@@ -441,11 +403,16 @@
 								 <option value="Instituto Centroamericano de Administracion de Empresas">SInstituto Centroamericano de Administracion de Empresas</option>				
 								 <option value="Otras Instituciones">Otras Instituciones</option>	
 							</select> 
-					    </div>					    
+					    </div>		
+					</div>
+					<div class="form-group">
+						<div class="col-sm-6 col-sm-offset-2">
+					    	{{ Form::text('titulo', Input::old('titulo'), array('class' => 'form-control', 'placeholder'=> '*Titulo')) }}	
+					    </div>    
 					</div>
 					<h3 class="subtitul">Año de Finalizacion</h3>
 					<div class="form-group">						
-					    <div class="col-sm-5">
+					    <div class="col-sm-6 col-sm-offset-2">
 					    	{{ Form::input('date','fecha_finalizacion', Input::old('fecha_finalizacion'), array('class' => 'form-control')) }}	
 					    </div>
 					</div>
@@ -457,7 +424,7 @@
 					<img src="{{ asset('img/bd.png') }}" alt="" class="computer">
 					OTROS ESTUDIOS
 				</h3>
-				<div class="campo">
+				<div class="campo col-sm-offset-2">
 					<div class="form-group">	
 					    <div class="col-sm-5">
 					    	<select class="form-control" name="idioma" >
@@ -468,9 +435,7 @@
 								 <option value="Chino - Mandarin">Chino - Mandarin</option>				
 								 <option value="Español">Español</option>	
 							</select> 
-					    </div>					    
-					</div>
-					<div class="form-group">	
+					    </div>
 					    <div class="col-sm-5">
 					    	<select class="form-control" name="nivel_dominio" >
 								 <option selected="selected" class="s">Nivel de Dominio </option>
@@ -493,9 +458,7 @@
 								 <option value="Hiberate">Hiberate</option>				
 								 <option value="Informix">Informix</option>	
 							</select> 
-					    </div>					    
-					</div>
-					<div class="form-group">	
+					    </div>
 					    <div class="col-sm-5">
 					    	<select class="form-control" name="frameworks" >
 								 <option selected="selected" class="s">FrameWorks</option>
@@ -517,9 +480,7 @@
 								 <option value="C">C</option>				
 								 <option value="C++">C++</option>	
 							</select> 
-					    </div>					    
-					</div>
-					<div class="form-group">	
+					    </div>
 					    <div class="col-sm-5">
 					    	<select class="form-control" name="programas_aplicacion" >
 								 <option selected="selected" class="s">Programas de Aplicación</option>
@@ -541,9 +502,7 @@
 								 <option value="Flash">Flash</option>				
 								 <option value="FreeHand">FreeHand</option>	
 							</select> 
-					    </div>					    
-					</div>
-					<div class="form-group">	
+					    </div>
 					    <div class="col-sm-5">
 					    	<select class="form-control" name="sistemas_operativos" >
 								 <option selected="selected" class="s">Sistemas Operativos</option>
@@ -557,10 +516,12 @@
 					</div>
 				</div>
 			</div>	
-			<div class="form-group">
-				<div class="col-sm-offset-2 col-sm-10">
-					{{ Form::submit('Guardar' , array('class'=> 'btn btn-primary')) }}
-				</div>	
+			<h3 class="divider"></h3>
+			<div class="alert alert-danger" role="alert">
+			  <p class="alert-link" style="text-align:center">LEER; Al darle registrar, esta aceptando los terminos y condiciones de nuestra, Politicas de privacidad</a>
+			</div>
+			<div class="form-group">								
+					{{ Form::submit('Crear Cuenta' , array('class'=> 'btn btn-primary regis')) }}				
 			</div>
 		{{ Form::close() }}	
 	</div>
