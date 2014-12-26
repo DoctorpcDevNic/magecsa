e14c6246e2491661f9009070e0915a435d443395
norwingc <norwingcruz@gmail.com>
2014-12-25 23:02:09 -0600

    mejoras en estilo login
    mejoras en estilo login

--- app/views/usuario/login.blade.php
+++ app/views/usuario/login.blade.php
@@ -2,7 +2,7 @@
 @section('contenido')
 	<div class="login">
 		<h3 class="titul">
-			Inicar Sesion
+			<i class="glyphicon glyphicon-user computer"></i>Inicar Sesion
 		</h3>
 		@if(Session::has('message'))
 			<div class="alert alert-info">{{ Session::get('message') }}</div>
@@ -10,14 +10,14 @@
 
 		{{ HTML::ul($errors->all(), array('class' =>'bg-danger')) }}    
 
-		{{ Form::open(array('url' => 'perfil/login', 'class' => 'form col-md-6 center-block')) }}
+		{{ Form::open(array('url' => 'perfil/login', 'class' => 'form col-md-6 col-sm-offset-3')) }}
 			<div class="form-group">
 				{{ Form::text('username', Input::old('username'), array('class' => 'form-control', 'placeholder'=> 'Username')) }} 
 			</div> 
 			<div class="form-group">
 				{{ Form::password('password', array('class' => 'form-control', 'placeholder'=> 'Password')) }}  
 			</div>
-			<div class="form-group">
+			<div class="form-group dow">
 				<button class="btn btn-primary btn-lg btn-block">Iniciar Sesion</button>
 				<span><a href="http://doctorpc.com.ni/" target="new">Olvide mi contraseña</a></span><br>
 				<span><a href="http://doctorpc.com.ni/" target="new">Registro de usuario</a></span>
